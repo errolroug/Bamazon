727a21c6402e354b13c9ba5bfa24ea0cc5f3cb7f
Errol Rougier <errolrougier@Errols-MBP.fios-router.home>
2019-03-25 10:47:51 -0400

    database setup and initial query

--- bamazon.sql
+++ bamazon.sql
@@ -1,1 +1,30 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+USE bamazon;
+
+CREATE TABLE products(
+  id INT(11) NOT NULL AUTO_INCREMENT ,
+  product_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(100) NOT NULL,
+  price DECIMAL(10) NOT NULL,
+  stock_quantity INTEGER(10) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('airpods', 'Electronics', 159.00, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('iPhone XR', 'Electronics', 749.00, 25);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Harry Potter and the Half Blood Prince','Books', 59.99, 35);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('couch','Furniture',569.99, 3);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("silica gels",'Miscellaneous',29.98, 300);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('nutribullet','Appliances',239.49,25);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('American Eagle Jeans','Apparel',45.95, 20);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Pacsun T-shirt','Apparel',19.99, 50);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('H&M sweater','Apparel',24.95, 62);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ('toaster oven','Appliances',59.99, 12);
+
+SELECT * FROM products;
+
+
+
 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,1 +1,35 @@
+var mysql = require("mysql");
+// var inquirer = require('inquirer')
 
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+   // Your port; if not 3306
+    port: 3306,
+   // Your username
+    user: "root",
+   // Your password
+    password: "password",
+    database: "bamazon"
+  });
+
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+ 
+  });
+
+//  
+
+  function queryAllProducts() {
+    connection.query("SELECT * FROM products", function(err, res) {
+      for (var i = 0; i < res.length; i++) {
+        console.log(res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price);
+      }
+      console.log("-----------------------------------");
+    
+    });
+  };
+
+  queryAllProducts();
