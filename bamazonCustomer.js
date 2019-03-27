1fbd9ca4bcfaf3cbcd0450d4f8ee619e34c2f25a
Errol Rougier <errolrougier@Errols-MBP.fios-router.home>
2019-03-27 10:49:39 -0400

    all my files

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,5 @@
 var mysql = require("mysql");
-// var inquirer = require('inquirer')
+var inquirer = require('inquirer')
 
 
 
@@ -14,22 +14,90 @@ var connection = mysql.createConnection({
     database: "bamazon"
   });
 
+
   connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
  
   });
 
-//  
 
-  function queryAllProducts() {
+//  This displays the table to the user and asks first question
+  function queryAllProducts(res) {
+      //First do the query and display it
     connection.query("SELECT * FROM products", function(err, res) {
       for (var i = 0; i < res.length; i++) {
         console.log(res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price);
       }
+
       console.log("-----------------------------------");
-    
-    });
+        //Then run an inquirer prompt and pass the query into it
+        idPrompt(res);
+    });  
   };
 
+
+  //First Question
+
+  function idPrompt(productInventory){
+      inquirer.prompt (
+        [{
+            type: "list",
+            name: "productID",
+            message: "Enter the ID of the item you would you like to purchase? ",
+            //validate provides true if answer is valid and a string if not
+            validate: function (input) {
+                // Declare function as asynchronous, and save the done cal
+                // Do async stuff
+                    setTimeout(function() {
+                        if (isNaN(input) ) {
+                            // Pass the return value in the done callback
+                            console.log('You need to provide an ID');
+                        }else{
+
+                        }
+                
+                
+                    }, 3000);
+                    };
+      }]//checks the input value from the customer
+      ).then(function (answer){
+          //turns it into a number
+        var inputID = parseInt(answer.productID);
+        //now we have both 
+        
+        function checkIfMatches(inputID,productInventory){
+                for(var i = 0; i < productInventory.length;i++){
+                    if(productInventory[i].id === inputID){
+                        return productInventory[i];
+                    }
+                };
+        };
+
+    checkIfMatches(inputID,productInventory);
+
+          
+      })
+  }
+
+
   queryAllProducts();
+
+
+  quantityPrompt = () => {
+    inquirer.prompt (
+        [{
+      type: "input",
+      name: "productQuantity",
+      message: "How many of this item would you like to purchase? [Quit with Q]",}]
+      ).then(function(){})
+
+
+
+ };
+
+
+ sufficientQuantityCheck = () => {
+
+
+ }
